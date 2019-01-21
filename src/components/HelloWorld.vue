5a2036d236eaa4cd6115c1a8dbffe80855530c2e
otera05 <otera05@gmail.com>
2019-01-21 14:57:15 +0900

    Todoの削除機能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,14 @@
   <div>
     {{ msg }}
     <form>
-      <button v-on:click="addTodo()">ADD TASK</button>
-      <button>DELETE FINISHED TASKS</button>
+      <button @click="addTodo()">ADD TASK</button>
+      <button @click="removeTodo()">DELETE FINISHED TASKS</button>
       <p>input: <input type="text" v-model="newTodo"></p>
       <p>task:{{ newTodo }}</p>
     </form>
     <div class="task-list">
       <label class="task-list__item" v-for="todo in todos">
-        <input type="checkbox"><button>EDIT</button>{{ todo.text }}
+        <input type="checkbox" v-model="todo.done"><button>EDIT</button>{{ todo.text }}
       </label>
     </div>
   </div>
@@ -42,6 +42,11 @@ export default {
       })
       this.newTodo = ''
     },
+    removeTodo: function (event) {
+      for (let i = this.todos.length - 1; i >= 0; i--) {
+        if (this.todos[i].done) this.todos.splice(i, 1)
+      }
+    }
   }
 }
 </script>
